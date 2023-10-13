89374218c78173ab98ea565f5228280c811044c0
Brian Capraro <Briancapraro0228@gmail.com>
2023-10-13 15:23:48 -0400

    sign up component

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+
+
+export default function SignUpForm(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event){
+    event.preventDefault();
+    try {
+        const url = 'https://fsa-jwt-practice.herokuapp.com/signup';
+        const response = await fetch(url)
+        const result = response.json()
+        console.log(result)
+    } catch(error){
+        console.log(error)
+    }
+    }
+    return (
+    <>
+        <h2>Sign up</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+               Username: <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </label><br />
+            <label>
+               Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </label><br />
+            <button>Submit</button>
+        </form>
+    </>
+    ) 
+      
+}
